40043d35eaf25b72b6faf44957fa196a3eccce90
abdussami398 <abdussami398@gmail.com>
2022-11-16 19:58:11 +0500

    Delete All and Edit Button and Functionality Added

--- app.js
+++ app.js
@@ -1,31 +1,45 @@
-var list = document.getElementById("task-list")
+var list = document.getElementById("task-list");
 
+const addvalue = () => {
+  let data = document.getElementById("user-input");
+  let li = document.createElement("li");
+  let list = document.getElementById("task-list");
+  let liText = document.createTextNode(data.value);
+  li.appendChild(liText);
 
-const add = () =>{
-    let data = document.getElementById("user-input");
-    let li = document.createElement("li")
-    let list = document.getElementById("task-list")
-    let liText = document.createTextNode(data.value)
-    li.appendChild(liText)
+  //Delete button
+  let delbtn = document.createElement("button");
+  let deltext = document.createTextNode("DELETE");
+  delbtn.setAttribute("class", "btn");
+  delbtn.setAttribute("onclick", "del(this)");
+  delbtn.appendChild(deltext);
 
+  //Edit Button
+  let editbtn = document.createElement("button");
+  let edittxt = document.createTextNode("EDIT");
+  delbtn.setAttribute("class", "btn");
+  editbtn.setAttribute("onclick", "edit(this)");
+  editbtn.appendChild(edittxt);
 
-    //Delete button 
-    let delbtn = document.createElement("button")
-    let deltext = document.createTextNode("DELETE")
-    delbtn.setAttribute("class","btn")
-    delbtn.setAttribute("onclick","del(this)")
-    delbtn.appendChild(deltext)
-    li.appendChild(delbtn)
-    list.appendChild(li)
-   
+  li.appendChild(editbtn);
+  li.appendChild(delbtn);
+  list.appendChild(li);
 
-    data.value = ""
-    console.log(li)
+  data.value = "";
+  console.log(li);
+};
 
-}
+const del = (e) => {
+  e.parentNode.remove();
+};
 
+const Deleteall = () => {
+  var list = document.getElementById("task-list");
+  list.innerHTML = "";
+};
 
-
-const del = (e) =>{
-    e.parentNode.remove()
-}
+const edit = (e) => {
+  let value = e.parentNode.firstChild.nodeValue;
+  let editvalue = prompt("Enter Edit Value", value);
+  e.parentNode.firstChild.nodeValue = editvalue;
+};
